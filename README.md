7895c7c99bb958467c3a97c2acdaa8aaa503e71c
Kishan <kishantiwari9713@gmail.com>
2020-11-17 22:39:09 +0530

    buildspec.yml & UpdateTaskdef.sh

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kube-app
-# kube-app
+
+# Links
+- https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_TaskDefinition.html
+- https://cloudaffaire.com/how-to-create-ecs-task-definition-using-aws-cli/
+- https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html
---
+++ UpdateTaskdef.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TASK_FAMILY="kube-app"
+SERVICE_NAME="kube-app-svc"
+NEW_DOCKER_IMAGE="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG"
+CLUSTER_NAME="kube-app"
+
+OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --output json)
+NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
+FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
+
+aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
+#aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME --profile obsdev
+
